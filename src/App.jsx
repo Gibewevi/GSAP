0e69a64ef0234b200225b40ed73398801141adf4
gibewevi <gibewevi@skiff.com>
2022-10-17 15:40:18 +0200

    update gsap

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,36 @@ import './App.css'
 
 function App() {
   const tl = gsap.timeline();
+
   useEffect(()=>{
+    const imgNav = document.querySelectorAll('nav img');
+    const allNavUl = document.querySelectorAll('nav ul');
+    const divImg = document.querySelectorAll('#logo img');
 
     tl
-    tl.to("#h1_vignes", {autoAlpha: 1, y:10,opacity:1, duration:0.75})
-    tl.to("#middle-line", {autoAlpha: 1, height:"150px", durantion:1.2})
-    tl.to("#grape_logo", {autoAlpha: 1, y:"15px", opacity:1, duration:0.75}, "-=0.2")
-    tl.to("#button_discover", {autoAlpha: 1,y:"12px", opacity:1, duration:0.4},"-=0.3")
-    .to("#ul_0", {autoAlpha: 1, duration:0.3, y:0, opacity:1})
-    .to("#ul_1", {autoAlpha: 1, duration:0.3, y:0, opacity:1}, "-=0.1")
-    .to("#ul_2", {autoAlpha: 1, duration:0.3, y:0, opacity:1}, "-=0.1")
-    .to("#barrel", {autoAlpha: 1, duration:0.5, y:0, opacity:0})
-    .to("#leaf", {autoAlpha: 1, duration:0.5, y:0, opacity:0}, "<")
-    .to("#twitter", {autoAlpha: 1, duration:0.5, opacity:1},"-=1.5")
-    .to("#facebook", {autoAlpha: 1, duration:0.5, opacity:1}, "-=1")
-    .to("#instagram", {autoAlpha: 1, duration:0.5, opacity:1}, "-=0.5")
-    .to("#copyright", {autoAlpha: 1, duration:0.5, opacity:1}, "-=1")
+    
+    .to("#h1_vignes", {autoAlpha: 1, y:10,opacity:1, duration:0.75, delay:0.2})
+    .to("#middle-line", {autoAlpha: 1, height:"150px", durantion:1.2})
+    .to("#grape_logo", {autoAlpha: 1, y:"15px", duration:0.75}, "-=0.2")
+    .to("#button_discover", {autoAlpha: 1,y:"12px", duration:0.4},"-=0.3")
+    .to("#copyright", {autoAlpha: 1, duration: 0.4})
+    .to(divImg, {autoAlpha: 1, duration:0.4, stagger:0.2}, '<')
+    .to(allNavUl, {autoAlpha: 1, y:0, duration: 0.4, stagger:0.1})
+    .to(imgNav, {autoAlpha: 1, y:0})
+
   })
 
   return (
     <div className="App w-full h-screen ">
   
         <nav className='p-4 flex flex-row justify-between items-center bg-gradient-to-b from-fuchsia-900 z-100 overflow-hidden'>
-            <img src="barrel.svg" id="barrel" className='w-7 -translate-y-[44px] opacity-0'/>
+            <img src="barrel.svg" id="barrel" className='w-7 -translate-y-[44px] opacity-1'/>
                 <div className='flex flex-row text-white font-light z-100 overflow-hidden'>
                     <ul id="ul_0" className='-translate-y-[18px]'>ACCEUIL</ul>
                     <ul id="ul_1" className='mx-5 -translate-y-[18px]'>LE VIN</ul>
                     <ul id="ul_2" className='-translate-y-[18px]'>CONTACT</ul>
                 </div>
-            <img src="leaf.svg" id="leaf" className='w-7 -translate-y-[44px] opacity-0'/>
+            <img src="leaf.svg" id="leaf" className='w-7 -translate-y-[44px] opacity-1'/>
         </nav>
 
         <div className='absolute top-0 w-full h-screen bg-black bg-opacity-60 -z-10' />
@@ -50,7 +51,7 @@ function App() {
 
           <div className='w-full absolute bottom-0'>
               <div className='max-w-7xl h-[60px] mx-auto flex items-center relative'>
-                  <div className='w-[180px]  flex flex-row p-2 items-center absolute right-0 mr-5'>
+                  <div id='logo' className='w-[180px]  flex flex-row p-2 items-center absolute right-0 mr-5'>
                       <img src="twitter.svg" id="twitter" className='w-8 opacity-0' />
                       <img src="facebook.svg" id="facebook" className='w-8 mx-5 opacity-0' />
                       <img src="instagram.svg" id="instagram"className='w-8 opacity-0' /> 
